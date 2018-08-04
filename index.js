ef28d2daa2d0b549f3862d189bc56303b95d9994
haojen ma <ehazomm@gmail.com>
2018-08-04 13:28:44 +0800

    add webpack

--- index.js
+++
@@ -1,6 +0,0 @@
-var main = require('./components/main.vue')
-module.exports =  {
-    install(Vue, options) {
-        Vue.component('zenlog', main.default)
-    }
-}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+export default class projectLOG {
+    constructor() {
+
+    }
+}
