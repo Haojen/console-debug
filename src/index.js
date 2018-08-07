79437252c81864c9b0def86715fb048080593118
haojen ma <ehazomm@gmail.com>
2018-08-08 00:09:22 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,19 +1,54 @@
 import {h, render, Component} from 'preact'
-import Statusbar from './statusbar'
 import styled from 'styled-components'
 
+const Consolebox = styled.div`
+    position: fixed;
+    left: 0;
+    top: 0;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    width: 100vw;
+    background: red;
+`
 
-const Header = styled.h1`
-  color: #F51963;
+const Menubar = styled.header`
+    display: flex;
+    height: 26px;
+    width: 100%;
+`
+
+const MenubarItem = styled.nav`
+    height: 100%;
+    margin: 0 5px;
+`
+
+const MainContainer = styled.section`
+    flex: 1;
+    border: 1px solid;
+    background-color: pink;
+`
+
+const Statusbar = styled.footer`
+    color: #F51963;
+    height: 20px;
+    background-color: black;
 `;
 
 
 class Console extends Component {
     render() {
         return (
-            <div>
-                <Header>123</Header>
-            </div>
+            <Consolebox>
+                <Menubar>
+                    <MenubarItem>Console</MenubarItem>
+                    <MenubarItem>Network</MenubarItem>
+                </Menubar>
+                <MainContainer>
+                    456
+                </MainContainer>
+                <Statusbar>123</Statusbar>
+            </Consolebox>
         )
     }
 }
