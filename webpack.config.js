fcb53c7f7322434ee6181ece04d352e21385b0df
haojen ma <ehazomm@gmail.com>
2018-08-09 23:45:11 +0800

    [modify]switch vue version

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,48 @@
 const HTMLWebpackPlugin = require('html-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-loader"
-                    },
-                    {
-                        loader: "sass-loader"
-                    }
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: './index.html',
-            filename: './index.html'
-        })
-    ],
-    resolve: {
-        alias: {
-            'react': 'preact-compat',
-            'react-dom': 'preact-compat',
-            // Not necessary unless you consume a module using `createClass`
-            'create-react-class': 'preact-compat/lib/create-react-class',
-            // Not necessary unless you consume a module requiring `react-dom-factories`
-            'react-dom-factories': 'preact-compat/lib/react-dom-factories'
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: {
+          loader: "vue-loader"
         }
-    }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.s[a|c]ss$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './index.html',
+      filename: './index.html'
+    }),
+    new VueLoaderPlugin()
+  ],
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+    },
+    extensions: ['*','.js', '.vue', '.json']
+  }
 }
