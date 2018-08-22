d1379c92d87cca3f3026b677dab17989d7cb60ac
haojen ma <ehazomm@gmail.com>
2018-08-22 17:43:24 +0800

    [update] support touch event on resizer

--- src/directive/resize/resize.js
+++ src/directive/resize/resize.js
@@ -2,10 +2,34 @@ export default {
   inserted(el, binding) {
     console.log(el, 'el')
     function resizer(obj) {
-      obj.onmousedown = e => {
+      const eventType = {
+        pc: {
+          startDrag: 'onmousedown',
+          startMove: 'onmousemove',
+          endDrag: 'onmouseup'
+        },
+
+        mobile: {
+          startDrag: 'ontouchstart',
+          startMove: 'ontouchmove',
+          endDrag: 'ontouchend'
+        },
+      }
+
+      const ev = eventType['ontouchstart' in document ? 'mobile' : 'pc']
+
+      obj[ev.startDrag] = e => {
+        let x = e.pageX,
+          y = e.pageY;
+
+        if (e.touches) {
+          x = e.touches[0].pageX
+          y = e.touches[0].pageY
+        }
+
         let dir = '';  //设置好方向
-        let firstX = e.clientX;  //获取第一次点击的横坐标
-        let firstY = e.clientY;   //获取第一次点击的纵坐标
+        let firstX = x;  //获取第一次点击的横坐标
+        let firstY = y;   //获取第一次点击的纵坐标
         let width = obj.offsetWidth;  //获取到元素的宽度
         let height = obj.offsetHeight;  //获取到元素的高度
         let Left = obj.offsetLeft;   //获取到距离左边的距离
@@ -23,29 +47,37 @@ export default {
           dir = "top";
         }
         //判断方向结束
-        document.onmousemove = e => {
+        document[ev.startMove] = e => {
+          let x = e.pageX,
+              y = e.pageY;
+
+          if (e.touches) {
+            x = e.touches[0].pageX
+            y = e.touches[0].pageY
+          }
+
           switch (dir) {
             case "right":
               document.body.style.cursor = 'ew-resize'
-              obj.style["width"] = width + (e.clientX - firstX) + "px";
+              obj.style["width"] = width + (x - firstX) + "px";
               break;
             case "left":
               document.body.style.cursor = 'ew-resize'
-              obj.style["width"] = width - (e.clientX - firstX) + "px";
-              obj.style["left"] = Left + (e.clientX - firstX) + "px";
+              obj.style["width"] = width - (x - firstX) + "px";
+              obj.style["left"] = Left + (x - firstX) + "px";
               break;
             case "top":
               document.body.style.cursor = 'ns-resize'
-              obj.style["height"] = height - (e.clientY - firstY) + "px";
-              obj.style["top"] = Top + (e.clientY - firstY) + "px";
+              obj.style["height"] = height - (y - firstY) + "px";
+              obj.style["top"] = Top + (y - firstY) + "px";
               break;
             case "down":
               document.body.style.cursor = 'ns-resize'
-              obj.style["height"] = height + (e.clientY - firstY) + "px";
+              obj.style["height"] = height + (y - firstY) + "px";
               break;
           }
         }
-        obj.onmouseup = () => {
+        obj[ev.endDrag] = () => {
           document.onmousemove = null
           document.body.style.cursor = 'default'
         }
